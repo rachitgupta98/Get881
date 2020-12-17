99dea3e8e42ea3db9cb45075039d55476917c365
rachitgupta98 <rachit371@gmail.com>
2020-12-17 17:07:39 +0530

    add show method

--- DemoApp/src/com/demo/testgit/Test.java
+++ DemoApp/src/com/demo/testgit/Test.java
@@ -1,6 +1,9 @@
 package com.demo.testgit;
 public class Test {
 
+	public static void show() {
+		System.out.println("Show");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
